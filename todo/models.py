780b27c1a50bc5dba5c6b45e521f9ee8746a5ad9
Tom1358 <thomasellis358@hotmail.com>
2021-10-24 17:05:11 +0000

    Create 'CR' of CRUD functionality

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    # CharField just contains characters or text
+    # null=False prevents items from being created without a name programmatically
+    # blank=False makes the field required on forms
+    done = models.BooleanField(null=False, blank=False, default=False)
+    # BooleanField can be either true or false
+
+    def __str__(self):
+        return self.name
+        # overrides Django string method to change how items are displayed
